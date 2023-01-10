9e6965ca15ae4ce6cff7cfd343a064af4af72602
Muhammad Moiz <106669492+MuhammadMoiz001@users.noreply.github.com>
2023-01-10 02:43:08 -0800

    Add files via upload

---
+++ billPayment.java
@@ -0,0 +1,31 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class billPayment implements bank{
+	Queue<Integer> q;
+	@Override
+	public void bill_Payment() {
+		// TODO Auto-generated method stub
+		q = new LinkedList<Integer>();
+		 for (int i = 1; i < 5; i++) 
+		 {
+		 q.add(i*1000); 
+		 }
+		for(Integer elem: q) {
+				System.out.println(elem + " Rs recived.");
+		}
+		
+	}
+	@Override
+	public void new_Account() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void loan_Takers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
